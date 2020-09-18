9c0734c6590311998aeb91c412ec93a9bc7f6fb9
Santiago L. Valdarrama <svpino@gmail.com>
2020-09-17 22:48:32 -0400

    added scheduler and cli

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # twitter-threading
 Automatically posting Twitter Threads
+
+Usage:
+
+```shell
+$ python main.py --thread thread.yaml --when "2020-09-17 22:44"
+```
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+from argparse import ArgumentError
+import os
+import re
 import tweepy
 import yaml
+import sched
+import time
+import datetime
+import argparse
+
+from dotenv import load_dotenv
 
 
-API_KEY = "YOUR API KEY GOES HERE"
-API_KEY_SECRET = "YOUR API KEY SECRET GOES HERE"
-ACCESS_TOKEN = "YOUR ACCESS TOKEN GOES HERE"
-ACCESS_TOKEN_SECRET = "YOUR ACCESS TOKEN SECRET GOES HERE"
+load_dotenv()
 
-USER = "YOUR USER GOES HERE"
+API_KEY = os.environ["API_KEY"]
+API_KEY_SECRET = os.environ["API_KEY_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+
+USER = os.environ["USER"]
 
 
 def authenticate():
@@ -16,19 +27,19 @@ def authenticate():
     return tweepy.API(auth, wait_on_rate_limit=True)
 
 
-def load():
-    with open(r'thread.yml') as file:
+def load(filename):
+    with open(filename) as file:
         thread = yaml.load(file, Loader=yaml.FullLoader)
 
-    return thread
+    return thread["thread"]
 
 
 def post(api, thread):
     status = None
-    for tweet in thread["thread"]:
+    for tweet in thread["tweets"]:
         args = {
             "status": tweet["text"],
-            "in_reply_to_status_id": (status.id if status else None)
+            "in_reply_to_status_id": (status.id if status else None),
         }
 
         if "attachment" in tweet:
@@ -39,7 +50,38 @@ def post(api, thread):
             status = api.update_status(**args)
 
 
+def schedule(api, thread, when):
+    when = datetime.datetime.strptime(when, "%Y-%m-%d %H:%M")
+    delta = (when - datetime.datetime.now()).total_seconds()
+
+    if delta < 0:
+        raise Exception("The value of the when argument is in the past")
+
+    schedule = sched.scheduler(time.time, time.sleep)
+    schedule.enter(delta, 60, post, kwargs={"api": api, "thread": thread})
+    schedule.run()
+
+
+def when_format(arg_value, pat=re.compile(r"(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})")):
+    if not pat.match(arg_value):
+        raise argparse.ArgumentTypeError("Invalid format. Expecting yyyy-mm-dd hh:mm")
+
+    return arg_value
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--thread", type=str, required=True, help="The yaml file containing the thread"
+    )
+    parser.add_argument(
+        "--when",
+        type=when_format,
+        required=True,
+        help="The date and time when the thread will be posted",
+    )
+    args = parser.parse_args()
+
     api = authenticate()
-    thread = load()
-    post(api, thread)
+    thread = load(args.thread)
+    schedule(api, thread, args.when)
